10645319c462beb981c8b65b195f281267a89460
Ruvain Maneth <cb012871@students.apiit.lk>
2026-05-20 13:21:19 +0530

    chore: update project changes

--- aerolink-frontend/src/config.js
+++ aerolink-frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://keozoo3dx4.execute-api.us-east-1.amazonaws.com";
+export const API_BASE_URL = "https://koezoo3dx4.execute-api.us-east-1.amazonaws.com";
 fetch(`${API_BASE_URL}/flights`)
